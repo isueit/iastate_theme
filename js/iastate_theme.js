62559a2f97adbe243d7b233f710466f1ff2f2577
Kristi Elmore <kelmore@iastate.edu>
2024-02-20 13:46:39 -0600

    Mobile menu changes

--- js/iastate_theme.js
+++ js/iastate_theme.js
@@ -8,7 +8,7 @@
 
 	// Toggle Menu Navbar and Site Links on Mobile
 	$('#isu-menu-navbar_toggler').click(function() {
-	  $('#isu-menu-navbar_toggler').toggleClass('isu-menu-navbar_toggler_open');
+	  $('#isu-menu-navbar_toggler').toggleClass('isu-menu-navbar_toggler_open isu-ext-mobile-menu');
 	  $('#isu-menu-navbar_collapse').toggleClass('isu-menu-navbar_show');
 	  $('#isu-sitelinks_collapse').toggleClass('isu-sitelinks_show');
 	});
