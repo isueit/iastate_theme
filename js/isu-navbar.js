005f5f320a53628367a268ffe26b260f6f3afd29
Kolby Moorman <eit_kmoor1@iastate.edu>
2025-01-28 10:17:33 -0600

    going to try and restart on new branch adding things from here to the new branch step by step

--- js/isu-navbar.js
+++ js/isu-navbar.js
@@ -3,111 +3,55 @@
  * jQuery the ISU Navbar. Includes keyboard navigable functionality.
  */
 
- (function ($, Drupal) {
+(function ($, Drupal) {
+  $(document).ready(function () {
+    // Toggle dropdown visibility
+    $(document).on('click', '.isu-dropdown-toggle', function (event) {
+      event.preventDefault();
 
-$(document).ready(function() {
+      var $toggle = $(this);
+      var $parentLi = $toggle.closest('li'); // Parent <li> element
+      var $dropdown = $parentLi.find('ul').first(); // Submenu <ul>
 
-  // Allow navigation with left and right arrow keys
-  $('#isu-navbar').on('keydown', function(event) {
-      if (event.keyCode === 39) { // right
-        event.preventDefault();
-        if ( $(':focus').is('.isu-navbar_dropdown-menu a') ) {
-          // If the focused item is a link in a dropdown...
-          // Then change the focus to the link in the next li
-          $(':focus').parent('li').next('li').children('a').focus();
-        } else if ( $(':focus').is('.isu-navbar_menu--left li:last-of-type a') ) {
-          // Or if the focused item is the last parent link in the first section (aka 'Index')...
-          // Change the focus to the link in the first li of the next section
-          $(':focus').closest('div').next('div').find('li:first-of-type a').focus();
-        } else {
-          // Finally, if the item is in the top level, but not the last item...
-          // Change the focus to the link in the next li
-          $(':focus').parent('li').next('li').children('a').focus();
-        }
+      // Check the current state
+      var isExpanded = $toggle.attr('aria-expanded') === 'true';
 
-      } else if (event.keyCode === 37) { // left
-        if ( $(':focus').is('.isu-navbar_alpha a') ) {
-          // If the item is in the Index dropdown...
-          // Change the focus to the link in the previous li
-          $(':focus').parent('li').prev('li').children('a').focus();
-        } else if ( $(':focus').is('.isu-navbar_menu--right > li:first-of-type a') ) {
-          // Or if the item is the first link in the second section aka 'Directory'
-          // Change the focus to the last link in the previous section
-          $(':focus').closest('div').prev('div').find('li:last-of-type a').focus();
-        } else {
-          // If the item is in the top level
-          $(':focus').parent().prev().children('a').focus();
-        }
+      // Toggle the current dropdown
+      $toggle.attr('aria-expanded', !isExpanded);
+      $dropdown.attr('aria-hidden', isExpanded ? 'true' : 'false');
 
+      // Close all other dropdowns
+      $parentLi.siblings().each(function () {
+        $(this).find('.isu-dropdown-toggle').attr('aria-expanded', 'false');
+        $(this).find('ul').attr('aria-hidden', 'true');
+      });
+    });
+
+    // Close dropdown with the back button
+    $(document).on('click', '.site-header__mega-menu-main-nav-dropdown-back', function (event) {
+      event.preventDefault();
+
+      var $dropdown = $(this).closest('ul'); // Current submenu
+      var $parentToggle = $dropdown.closest('li').find('.isu-dropdown-toggle'); // Parent toggle
+
+      // Close the submenu
+      $dropdown.attr('aria-hidden', 'true');
+      $parentToggle.attr('aria-expanded', 'false');
+      $parentToggle.focus(); // Focus the parent toggle
+    });
+
+    // Keep child links functional
+    $(document).on('click', '.link-territory', function (event) {
+      console.log('Navigating to:', event.target.href); // Debugging navigation
+    });
+
+    // Close dropdowns on outside click
+    $(document).on('click', function (event) {
+      if (!$(event.target).closest('.isu-dropdown-toggle, .isu-navbar_dropdown').length) {
+        // Close all dropdowns
+        $('.isu-dropdown-toggle').attr('aria-expanded', 'false');
+        $('.isu-navbar_dropdown ul').attr('aria-hidden', 'true');
       }
+    });
   });
-
-  // Traverse Signons menu with up/down arrow keys
-  $('.isu-navbar_signons').on('keydown', function(event) {
-      if (event.keyCode === 40) { // down
-        event.preventDefault();
-        // Change the focus to the link in the next li
-        $(':focus').parent('li').next('li').children('a').focus();
-      } else if (event.keyCode == 38) { // up
-        if ( $(':focus').is('.isu-navbar_signons li:not(:first-of-type) a') ) {
-          // If the focus is NOT the first item in the list...
-          // Change the focus to the link in the previous li
-          $(':focus').parent('li').prev().children('a').focus();
-        }
-      }
-  })
-
-  // Enter dropdowns with the down arrow
-  $('.isu-navbar_dropdown > .isu-navbar_dropdown-toggle').on('keydown', function(event) {
-    var dropdownToggle = $(this);
-      if (event.keyCode === 40) { // down
-        event.preventDefault();
-        // Add class for styling
-        dropdownToggle.addClass('has-focus');
-        // Open menu
-        dropdownToggle.parents('.isu-navbar_dropdown').attr('aria-expanded', 'true');
-        // Change focus to first link in the dropdown
-        $(':focus').next().find('li:first-of-type a').focus();
-      }
-  });
-
-  // Exit the Index submenu with up arrow
-  $('.isu-navbar_alpha > li > a').on('keydown', function(event) {
-    var dropdownMenuItem = $(this);
-      if (event.keyCode === 38) { // up
-        // Close the dropdown
-        dropdownMenuItem.closest('.isu-navbar_dropdown').attr('aria-expanded', 'false');
-        // Refocus on the parent link
-        dropdownMenuItem.closest('.isu-navbar_dropdown').find('a').focus();
-      }
-  });
-
-  // Exit the Sign-Ons menu with up arrow
-  $('.isu-navbar_signons > li:first-of-type a').on('keydown', function(event) {
-    var dropdownMenuItem = $(this);
-      if (event.keyCode === 38) { // up
-        // Close the dropdown
-        dropdownMenuItem.closest('.isu-navbar_dropdown').attr('aria-expanded', 'false');
-        // Refocus on the parent link.
-        dropdownMenuItem.closest('.isu-navbar_dropdown').find('a').focus();
-      }
-  });
-
-  // Close dropdowns upon exit
-  $('.isu-navbar_dropdown-menu').on('mouseleave focusout', function(e) {
-    var dropdownMenu = $(this);
-    setTimeout(function() {
-      if ( dropdownMenu.find(':focus').length === 0) {
-        // If the dropdown nor its children don't not have focus...
-        if ( dropdownMenu.siblings('a:focus').length === 0) {
-          // Close the menu
-          dropdownMenu.parents('.isu-navbar_dropdown').attr('aria-expanded', 'false');
-          // And remove styling class
-          dropdownMenu.parents('.isu-navbar_dropdown').removeClass('has-focus');
-        }
-      }
-    }, 100 );
-  })
-})
-
 })(jQuery, Drupal);
